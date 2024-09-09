af9696a593e80f492d3f2cb50798bc58192a689a
RashidulIslam0 <aboutrashidul@gmail.com>
2024-09-09 15:59:41 +0600

    AuthMiddleware

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,12 @@ import express from "express";
 const router=express.Router()
 import * as TaskController from "../app/controllers/TaskController.js";
 import * as UserController from "../app/controllers/UserController.js";
+import AuthMiddleware from "../app/middlewares/AuthMiddleware.js";
 
 // USERS
 router.post("/Registration",UserController.Registration)
 router.post("/Login",UserController.Login)
-router.get("/ProfileDetails",AbortController,UserController.ProfileDetails)
+router.get("/ProfileDetails",AuthMiddleware,UserController.ProfileDetails)
 router.post("/CodeVerify",UserController.CodeVerify)
 router.post("/ProfileUpdate",UserController.ProfileUpdate)
 router.post("/EmailVerify",UserController.EmailVerify)
